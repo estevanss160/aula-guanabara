1db7526ec9a4adfce48dcadcd5fbffb7c2368963
Estêvan Silva <esteevan160@gmail.com>
2026-01-30 14:00:58 -0300

    formularios

---
+++ HTML5 e CSS3/Guanabara 4/exercicio 25 formulário/cadastro.php
@@ -0,0 +1,7 @@
+os dasos foram enviados
+aprenda PHP,JAVA,JAVASCRIPT,PYTHON ou outra linguagem para saber o que fazer
+
+
+データが送信されました。
+PHP、Java、JavaScript、Python、またはその他の言語を学習して、何をすべきか理解しましょう。
+Dēta ga sōshin sa remashita. PHP, jawa, jabasukuriputo, paison, matawa sonohoka no gengo o gakushū shite, nani o subeki ka rikai shimashou.
